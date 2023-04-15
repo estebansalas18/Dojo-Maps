d80891535f8e5b37bab06a7edb107122f0916236
Esteban Salas <juan.salasf@udea.edu.co>
2023-04-15 00:46:32 -0500

    Add: mapa

---
+++ app/src/main/java/com/example/mapa/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.mapa;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener{
+
+    EditText txtLatitud, txtLongitud;
+    GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtLatitud = findViewById(R.id.txtLatitud);
+        txtLongitud = findViewById(R.id.txtLongitud);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        this.mMap.setOnMapClickListener(this);
+        this.mMap.setOnMapLongClickListener(this);
+
+        LatLng med = new LatLng(6.217 ,-75.567 );
+        mMap.addMarker(new MarkerOptions().position(med).title("Medellín"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(med));
+    }
+
+    @Override
+    public void onMapClick(@NonNull LatLng latLng) {
+        txtLatitud.setText(String.valueOf(latLng.latitude));
+        txtLongitud.setText(String.valueOf(latLng.longitude));
+
+        mMap.clear();
+        LatLng map = new LatLng(latLng.latitude,latLng.longitude);
+        mMap.addMarker(new MarkerOptions().position(map).title(""));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(map));
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+        txtLatitud.setText(String.valueOf(latLng.latitude));
+        txtLongitud.setText(String.valueOf(latLng.longitude));
+
+        mMap.clear();
+        LatLng map = new LatLng(latLng.latitude,latLng.longitude);
+        mMap.addMarker(new MarkerOptions().position(map).title(""));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(map));
+    }
+}
